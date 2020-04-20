0a50e228eaf4cc4399af511cabf91258c7e05e6e
Aaron Jorgensen <mraaronjorgensen@gmail.com>
2020-04-19 22:28:54 -0700

    improved product search

--- app.py
+++ app.py
@@ -1,23 +1,22 @@
-import os
+
 import csv
 from datetime import datetime
 from collections import OrderedDict
 
-import openpyxl
 import pandas as pd
 from peewee import *
 
 from settings import *
 from welcome import *
 
-db = SqliteDatabase('inventory.db')
+db = SqliteDatabase(DATABASE)
 
 class Product(Model):
     product_id = AutoField()
-    date_updated = DateTimeField(default=datetime.now)
     product_name = TextField()
-    product_quantity = IntegerField()
     product_price = IntegerField()
+    product_quantity = IntegerField()
+    date_updated = DateTimeField()
 
     class Meta:
         database = db
@@ -28,7 +27,7 @@ def initialize():
 
 
 def open_and_clean_csv():
-    with open('inventory.csv', newline='') as csvfile:
+    with open(SOURCE, newline='') as csvfile:
         inv_reader = csv.DictReader(csvfile, delimiter=',')
         rows = list(inv_reader)
         for row in rows[:]:
@@ -51,101 +50,102 @@ def open_and_clean_csv():
                                product_quantity=row['product_quantity'],
                                date_updated=row['date_updated'])
 
+
 def backup():
-	"""backup db"""
-	df = pd.read_sql("SELECT * FROM Product;", db)
-	print("Which format would you like to export to?")
-	print("""
+    """backup db"""
+    print("Which format would you like to export to?")
+    print("""
 [1] .json
 [2] .csv
 [3] .xlsx
-[4] all formats
-	""")
-	export_type = input("___:")
-	if export_type == '1':
-		df.to_json('backups/backup.json', orient='records', lines=True)
-		print("Backup saved")
-	elif export_type == '2':
-		df.to_csv('backups/backup.csv', sep='\t', index=False)
-		print("Backup saved")
-	elif export_type == '3':
-		writer = pd.ExcelWriter('backups/backup.xlsx')
-		df.to_excel(writer, 'DataFrame')
-		writer.save()
-		print("Backups saved")
-	else:
-		print("You didn't enter a valid option")
+    """)
+    export_type = input("___:")
+    if export_type == '1':
+        df.to_json('backups/backup.json', orient='records', lines=True)
+        print(".json Backup saved")
+    elif export_type == '2':
+        df.to_csv('backups/backup.csv', sep='\t', index=False)
+        print(".csv Backup saved")
+    elif export_type == '3':
+        writer = pd.ExcelWriter('backups/backup.xlsx')
+        df.to_excel(writer, 'DataFrame', index=False)
+        writer.save()
+        print(".xlsx Backup saved")
+    else:
+        print("You didn't enter a valid option")
 
 
 def view_every_product():
-	"""Display all products"""
-	choice = int(input("Pick a number"))
-	result = df.iloc[choice]
+    """Display all products"""
+    view_all = Product.select()
 
-	print(result)
+    print(view_all)
 
 def add_product():
-	"""add a product"""
-	input_name = str(input("Enter a name: "))
-	input_qty = int(input("enter a quantity"))
-	input_price = input("Enter a price")
-	new_date = DateTimeField(default=datetime.now())
-	data_source = [
-			{'product_name': input_name, 'product_price': input_price},
-			{'product_quantity': input_qty, 'date_updated': new_date}
-	]
+    """add a product"""
+    input_name = str(input("Enter a name: "))
+    input_qty = int(input("enter a quantity"))
+    input_price = input("Enter a price")
+    fields = [Product.product_name, Product.product_price, Product.product_quantity, Product.date_updated]
+    data = [(input_name, input_price, input_qty, datetime.today()
+                    )]
 
-	for data_dict in data_source:
-		Product.create(**data_dict)
+    Product.insert_many(data, fields=fields).execute()
+
 
 def search_product():
-	"""Search for a product (i.e 1)"""
-	total_rows = Product.select().count()
-	try:
-		product_id = int(input(f'Input product id (hint: between 1 and {total_rows}:) '))
-		# http://docs.peewee-orm.com/en/latest/peewee/api.html#Model.get_by_id
-		product = Product.get_by_id(product_id)
-		print("""
+    """Search for a product (i.e 1)"""
+    clear()
+    total_rows = Product.select().count()
+    try:
+        product_id = int(input(f'Input product id (hint: between 1 and {total_rows}:) '))
+        # http://docs.peewee-orm.com/en/latest/peewee/api.html#Model.get_by_id
+        product = Product.get_by_id(product_id)
+        print("""
     Your search result is:
-		""")
-		print(f'NAME --------------|{product.product_name}')
-		print('PRICE -------------|${}'.format(product.product_price / 100))
-		print(f'QTY ---------------|{product.product_quantity}')
-		print(f'DATE UPDATED ------|{product.date_updated}')
-	except ValueError:
-		print(f'Please enter a number value from 1 to {total_rows}')
-	except Product.DoesNotExist:
-		print("The product does not exist")
+        """)
+        print(f'NAME --------------|{product.product_name}')
+        print('PRICE -------------|${}'.format(product.product_price / 100))
+        print(f'QTY ---------------|{product.product_quantity}')
+        print(f'DATE UPDATED ------|{product.date_updated}')
+    except ValueError:
+        print(f'Please enter a number value from 1 to {total_rows}')
+    except Product.DoesNotExist:
+        print(f"""The product does not exist.
+{product_id} is not within 1 to {total_rows}
+""")
+        clear()
 
 
 
 def menu_loop():
-	"""show the menu"""
-	choice = None
+    """show the menu"""
+    choice = None
 
-	while choice != 'q':
-		print("Enter 'q' to quit.")
-		for key, value in menu.items():
-			print('{}) {}'.format(key, value.__doc__))
-		choice = input('Action: ').lower().strip()
+    while choice != 'q':
+        print("Enter 'q' to quit.")
+        for key, value in menu.items():
+            print('{}) {}'.format(key, value.__doc__))
+        choice = input('Action: ').lower().strip()
 
-		if choice in menu:
-			menu[choice]()
+        if choice in menu:
+            menu[choice]()
 
 
 menu = OrderedDict([
-		('a', add_product),
-		('b', backup),
-		('e', view_every_product),
-		('v', search_product)
+        ('a', add_product),
+        ('b', backup),
+        ('e', view_every_product),
+        ('v', search_product)
 ])
 
 if __name__ == '__main__':
-	loading()
-	os.system('cls' if os.name == 'nt' else 'clear')
-	welcome()
-	initialize()
-	open_and_clean_csv()
-	menu_loop()
-
-# backup()
+    try:
+        loading()
+        welcome()
+        initialize()
+        open_and_clean_csv()
+        menu_loop()
+    except KeyboardInterrupt:
+        clear()
+        print("\nThanks for dropping by!")
--- settings.py
+++ settings.py
@@ -1,20 +1,24 @@
+import os
+
 from playhouse.sqlite_ext import *
 import pandas as pd
-# Database file/configuration
 
+# Database file/configuration
 DATABASE = 'inventory.db'
 db = SqliteDatabase(DATABASE)
 
+#Pandas dataframe
 df = pd.read_sql("SELECT * FROM Product;", db)
+
 # source product file
 SOURCE = 'inventory.csv'
 
-# Backup files
 
-# BACKUP_JSON = df.to_json('backups/backup.json', orient='records', lines=True)
-# BACKUP_CSV = df.to_csv('backups/backup.csv', sep='\t', index=False)
-# BACKUP_DB = df.to_sql('backups/backup.db', db, index_label=False, if_exists='replace')
-#BACKUP_ALL = BACKUP_JSON BACKUP_CSV
+def clear():
+	"""Clear the screen"""
+	os.system('cls' if os.name == 'nt' else 'clear')
+
+
 
 
 
